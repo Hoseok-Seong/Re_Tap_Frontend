5fb346d8a1f14010d7e811f6dc5c8ab922868b8d
Hoseok Seong <shsshs0125@naver.com>
2025-06-10 23:31:46 +0900

    git ignore

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppConstant {
-  static const String baseUrl = 'http://localhost:8080';
+  static const String baseUrl = 'http://3.39.223.153:80';
 }
 
 class AppColors {
